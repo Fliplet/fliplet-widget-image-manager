def7cbe88fdff43b7645add4695950e5462f8c5a
Hugo Carneiro <hcarneiro@weboo.biz>
2017-03-08 18:24:21 +0000

    Updated column design

--- js/interface.templates.js
+++ js/interface.templates.js
@@ -7,11 +7,9 @@ this["Fliplet"]["Widget"]["Templates"]["templates.app"] = Handlebars.template({"
 
   return "<div class=\"col-xs-4 item-holder folder\" data-app-id=\""
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
-    + "\">\n    <div class=\"image-holder\">\n        <div class=\"image-overlay\">\n            <i class=\"fa fa-folder\" aria-hidden=\"true\"></i>\n        </div>\n        <div class=\"image-info\">\n            <p>\n                <strong>"
+    + "\">\n    <div class=\"image-holder\">\n        <div class=\"image-overlay\">\n            <i class=\"fa fa-folder\" aria-hidden=\"true\"></i>\n        </div>\n    </div>\n    <div class=\"image-title\">\n        <p>\n            "
     + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
-    + "</strong>\n            </p>\n            <p>\n                Double click to open the folder\n            </p>\n        </div>\n    </div>\n    <div class=\"image-title\">\n        <p>\n            <strong>"
-    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
-    + "</strong>\n        </p>\n    </div>\n</div>";
+    + "\n        </p>\n    </div>\n</div>\n";
 },"useData":true});
 
 this["Fliplet"]["Widget"]["Templates"]["templates.file"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
@@ -21,11 +19,9 @@ this["Fliplet"]["Widget"]["Templates"]["templates.file"] = Handlebars.template({
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
     + "\">\n    <div class=\"image-holder\" style=\"background-image: url('"
     + alias4(((helper = (helper = helpers.thumbnail || (depth0 != null ? depth0.thumbnail : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"thumbnail","hash":{},"data":data}) : helper)))
-    + "');\">\n        <div class=\"image-info\">\n            <p>\n                <strong>"
+    + "');\"></div>\n    <div class=\"image-title\">\n        <p>\n            "
     + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
-    + "</strong>\n            </p>\n        </div>\n    </div>\n    <div class=\"image-title\">\n        <p>\n            <strong>"
-    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
-    + "</strong>\n        </p>\n    </div>\n</div>";
+    + "\n        </p>\n    </div>\n</div>\n";
 },"useData":true});
 
 this["Fliplet"]["Widget"]["Templates"]["templates.folder"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
@@ -35,11 +31,9 @@ this["Fliplet"]["Widget"]["Templates"]["templates.folder"] = Handlebars.template
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
     + "\" data-parent-id=\""
     + alias4(((helper = (helper = helpers.parentId || (depth0 != null ? depth0.parentId : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"parentId","hash":{},"data":data}) : helper)))
-    + "\" }>\n    <div class=\"image-holder\">\n        <div class=\"image-overlay\">\n            <i class=\"fa fa-folder\" aria-hidden=\"true\"></i>\n        </div>\n        <div class=\"image-info\">\n            <p>\n                <strong>"
+    + "\" }>\n    <div class=\"image-holder\">\n        <div class=\"image-overlay\">\n            <i class=\"fa fa-folder\" aria-hidden=\"true\"></i>\n        </div>\n    </div>\n    <div class=\"image-title\">\n        <p>\n            "
     + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
-    + "</strong>\n            </p>\n            <p>\n                Double click to open the folder\n            </p>\n        </div>\n    </div>\n    <div class=\"image-title\">\n        <p>\n            <strong>"
-    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
-    + "</strong>\n        </p>\n    </div>\n</div>";
+    + "\n        </p>\n    </div>\n</div>\n";
 },"useData":true});
 
 this["Fliplet"]["Widget"]["Templates"]["templates.nofiles"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
@@ -51,9 +45,7 @@ this["Fliplet"]["Widget"]["Templates"]["templates.organization"] = Handlebars.te
 
   return "<div class=\"col-xs-4 item-holder folder\" data-organization-id=\""
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
-    + "\">\n    <div class=\"image-holder\">\n        <div class=\"image-overlay\">\n            <i class=\"fa fa-folder\" aria-hidden=\"true\"></i>\n        </div>\n        <div class=\"image-info\">\n            <p>\n                <strong>"
+    + "\">\n    <div class=\"image-holder\">\n        <div class=\"image-overlay\">\n            <i class=\"fa fa-folder\" aria-hidden=\"true\"></i>\n        </div>\n    </div>\n    <div class=\"image-title\">\n        <p>\n            "
     + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
-    + "</strong>\n            </p>\n            <p>\n                Double click to open the folder\n            </p>\n        </div>\n    </div>\n    <div class=\"image-title\">\n        <p>\n            <strong>"
-    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
-    + "</strong>\n        </p>\n    </div>\n</div>";
+    + "\n        </p>\n    </div>\n</div>\n";
 },"useData":true});
